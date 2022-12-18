b37e96e3d9878c3cf483d5d57c31a5032806c23a
philippebazinet <gestionbazinet@gmail.com>
2022-12-18 14:01:44 -0500

    singleton classes

---
+++ bin/dart_singleton_class.dart
@@ -0,0 +1,24 @@
+/// SINGLETON PATTERN ***********************************************
+///
+///the main function of every dart program is main()
+void main(List<String> arguments) {
+  ///all rocket instances below point to the same instance
+  var rocket = Rocket();
+  var rocket2 = Rocket();
+  var rocket3 = Rocket();
+
+}
+
+class Rocket{
+  
+  //we disable default constructor by creating a singleton constructor
+  Rocket._();
+
+  //we create an instance of a Rocket
+  static final Rocket _instance = Rocket._();
+
+  //we create a factory to instanciate the instance
+  factory Rocket() {
+    return _instance;
+  }
+}
